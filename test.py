18325d29bfca5e864d61e1a50916c930020fa678
Leon V <33411715+leonv024@users.noreply.github.com>
2018-08-04 23:49:20 +0200

    Added progressbar

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os, sys, requests, argparse, http, datetime, socket
+from tqdm import tqdm
 from prettytable import PrettyTable
 
 def parse_args():
@@ -18,17 +19,21 @@ c = 0
 def run(domain):
     global c
 
-    print("\033[32mRunning...\033[0m")
-
+    #print("\033[32mRunning...\033[0m")
+    tqdm.write('\033[32mRunning...\033[0m')
     try:
-        print('\033[33mChecking %s:80\033[0m' % (domain))
+        #print('\033[33mChecking %s:80\033[0m' % (domain))
+        tqdm.write('\033[33mChecking %s:80\033[0m' % (domain))
+
         h1 = http.client.HTTPConnection(domain, timeout=3)
         h1.request("GET", "/")
         r1 = h1.getresponse()
         http_status = '%s %s' % (r1.status, r1.reason)
-        print('HTTP status: %s' % http_status)
+        #print('HTTP status: %s' % http_status)
+        tqdm.write('HTTP status: %s' % http_status)
     except socket.timeout:
-        print('Timeout Reached for domain %s' % domain)
+        #print('Timeout Reached for domain %s' % domain)
+        tqdm.write('Timeout Reached for domain %s' % domain)
         http_status = 'Timed out'
         pass
     except Exception:
@@ -41,15 +46,18 @@ def run(domain):
         pass
 
     try:
-        print('\033[33mChecking %s:443\033[0m' % (domain))
+        #print('\033[33mChecking %s:443\033[0m' % (domain))
+        tqdm.write('\033[33mChecking %s:443\033[0m' % (domain))
         h2 = http.client.HTTPSConnection(domain, timeout=3)
         h2.request("GET", "/")
         r2 = h2.getresponse()
         https_status = '%s %s' % (r2.status, r2.reason)
-        print('HTTPS status: %s' % https_status)
+        #print('HTTPS status: %s' % https_status)
+        tqdm.write('HTTPS status: %s' % https_status)
     # HTTPS is the 2nd and final timeout error. Return results if it happends and contimue.
     except socket.timeout:
-        print('Timeout Reached for domain %s' % domain)
+        #print('Timeout Reached for domain %s' % domain)
+        tqdm.write('Timeout Reached for domain %s' % domain)
         https_status = 'Timed out'
         https = False
         redirect = False
@@ -57,7 +65,8 @@ def run(domain):
         result = domain, http_status, https_status, https, redirect, xss_protection
         return result
     except Exception:
-        print('\033[31mA SSL Error occured for domain %s\033[0m' % domain)
+        #print('\033[31mA SSL Error occured for domain %s\033[0m' % domain)
+        tqdm.write('\033[31mA SSL Error occured for domain %s\033[0m' % domain)
         https_status = 'SSL Error'
         https = False
         redirect = False
@@ -68,30 +77,37 @@ def run(domain):
 
     try:
         if r1.status != 200 and r2.status != 200 and domain.startswith('www.'):
-            print("\033[33mBoth HTTP and HTTPS seem to be unreachable for %s, check the domain. Trying again, stripping www.\033[0m" % domain)
+            #print("\033[33mBoth HTTP and HTTPS seem to be unreachable for %s, check the domain. Trying again, stripping www.\033[0m" % domain)
+            tqdm.write("\033[33mBoth HTTP and HTTPS seem to be unreachable for %s, check the domain. Trying again, stripping www.\033[0m" % domain)
             if domain.startswith('www.'):
                 try:
                     domain = domain[4:] # Strip www.
-                    print('\033[33mChecking %s:80\033[0m' % (domain))
+                    #print('\033[33mChecking %s:80\033[0m' % (domain))
+                    tqdm.write('\033[33mChecking %s:80\033[0m' % (domain))
                     h1 = http.client.HTTPConnection(domain, timeout=3)
                     h1.request("GET", "/")
                     r1 = h1.getresponse()
                     http_status = '%s %s' % (r1.status, r1.reason)
-                    print('HTTP status: %s' % http_status)
+                    #print('HTTP status: %s' % http_status)
+                    tqdm.write('HTTP status: %s' % http_status)
                 except socket.timeout:
-                    print('Timeout Reached for domain %s' % domain)
+                    #print('Timeout Reached for domain %s' % domain)
+                    tqdm.write('Timeout Reached for domain %s' % domain)
                     http_status = 'Timed out'
                     pass
 
                 try:
-                    print('\033[33mChecking %s:443\033[0m' % (domain))
+                    #print('\033[33mChecking %s:443\033[0m' % (domain))
+                    tqdm.write('\033[33mChecking %s:443\033[0m' % (domain))
                     h2 = http.client.HTTPSConnection(domain, timeout=3)
                     h2.request("GET", "/")
                     r2 = h2.getresponse()
                     https_status = '%s %s' % (r2.status, r2.reason)
-                    print('HTTPS status: %s' % https_status)
+                    #print('HTTPS status: %s' % https_status)
+                    tqdm.write('HTTPS status: %s' % https_status)
                 except socket.timeout:
-                    print('Timeout Reached for domain %s' % domain)
+                    #print('Timeout Reached for domain %s' % domain)
+                    tqdm.write('Timeout Reached for domain %s' % domain)
                     https_status = 'Timed out'
                     https = False
                     redirect = False
@@ -101,30 +117,37 @@ def run(domain):
 
 
         elif r1.status != 200 and r2.status != 200:
-            print("\033[33mBoth HTTP and HTTPS seem to be unreachable for %s, check the domain: trying again, adding 'www.'.\033[0m" % domain)
+            #print("\033[33mBoth HTTP and HTTPS seem to be unreachable for %s, check the domain: trying again, adding 'www.'.\033[0m" % domain)
+            tqdm.write("\033[33mBoth HTTP and HTTPS seem to be unreachable for %s, check the domain: trying again, adding 'www.'.\033[0m" % domain)
             if not domain.startswith('www.'):
                 try:
                     domain = 'www.' + domain
-                    print('\033[33mChecking %s:80\033[0m' % (domain))
+                    #print('\033[33mChecking %s:80\033[0m' % (domain))
+                    tqdm.write('\033[33mChecking %s:80\033[0m' % (domain))
                     h1 = http.client.HTTPConnection(domain, timeout=3)
                     h1.request("GET", "/")
                     r1 = h1.getresponse()
                     http_status = '%s %s' % (r1.status, r1.reason)
-                    print('HTTP status: %s' % http_status)
+                    #print('HTTP status: %s' % http_status)
+                    tqdm.write('HTTP status: %s' % http_status)
                 except socket.timeout:
-                    print('Timeout Reached for domain %s' % domain)
+                    #print('Timeout Reached for domain %s' % domain)
+                    tqdm.write('Timeout Reached for domain %s' % domain)
                     http_status = 'Timed out'
                     pass
 
                 try:
-                    print('\033[33mChecking %s:443\033[0m' % (domain))
+                    #print('\033[33mChecking %s:443\033[0m' % (domain))
+                    tqdm.write('\033[33mChecking %s:443\033[0m' % (domain))
                     h2 = http.client.HTTPSConnection(domain, timeout=3)
                     h2.request("GET", "/")
                     r2 = h2.getresponse()
                     https_status = '%s %s' % (r2.status, r2.reason)
-                    print('HTTPS status: %s' % https_status)
+                    #print('HTTPS status: %s' % https_status)
+                    tqdm.write('HTTPS status: %s' % https_status)
                 except socket.timeout:
-                    print('Timeout Reached for domain %s' % domain)
+                    #print('Timeout Reached for domain %s' % domain)
+                    tqdm.write('Timeout Reached for domain %s' % domain)
                     https_status = 'Timed out'
                     https = False
                     redirect = False
@@ -132,7 +155,8 @@ def run(domain):
                     result = domain, http_status, https_status, https, redirect, xss_protection
                     return result
     except Exception as e:
-        print('\033[36m%s\033[0m' % e)
+        #print('\033[36m%s\033[0m' % e)
+        tqdm.write('\033[36m%s\033[0m' % e)
 
     try:
         if r2.status == 200:
@@ -148,11 +172,19 @@ def run(domain):
         r = requests.get('http://' + domain)
 
         if r.history:
-            print("Request was redirected")
-            for resp in r.history:
-                print(resp.status_code, resp.url)
-            print("Final destination:")
-            print(r.status_code, r.url)
+            #print("Request was redirected")
+            tqdm.write("Request was redirected")
+
+            # These debug lines somehow break the redirect test, therefor they were removed.
+            # tqdm.write() seems to break it. Less debug, big deal.
+            #for resp in r.history:
+                #print(resp.status_code, resp.url)
+                #tqdm.write(resp.status_code, resp.url)
+            #print("Final destination:")
+            #tqdm.write("Final destination:")
+            #print(r.status_code, r.url)
+            #tqdm.write(r.status_code, r.url)
+
             if r.url.startswith('https://') and r.status_code == 200:
                 redirect = True
                 if not https == True:
@@ -166,7 +198,8 @@ def run(domain):
                 redirect = False
 
         else:
-            print("Request was not redirected")
+            #print("Request was not redirected")
+            tqdm.write("Request was not redirected")
             redirect = False
     except Exception:
         redirect = False
@@ -185,7 +218,8 @@ def run(domain):
         else:
             xss_protection = False
 
-    print('X-XSS-Protection: %s' % xss_protection)
+    #print('X-XSS-Protection: %s' % xss_protection)
+    tqdm.write('X-XSS-Protection: %s' % xss_protection)
 
     h1.close(); h2.close() # Close connections
 
@@ -218,19 +252,21 @@ def test_domains(file):
 
     try:
         f = open(file).readlines()
+        
+        with tqdm(total=(len(f)), desc='Progress') as bar:
+            for domain in f:
+                domain = domain.strip() # Strip \n and b' and stuff
+                result = run(domain) # Run test
+                bar.update(1)
+                table.add_row([result[0], result[1], result[2], result[3], result[4], result[5]])
 
-        for domain in f:
-            domain = domain.strip() # Strip \n and b' and stuff
-            result = run(domain) # Run test
+            # Calculate how much percentage is HTTPS and error.
+            secure = 100 / len(f) * c
 
-            table.add_row([result[0], result[1], result[2], result[3], result[4], result[5]])
+            print(table) # Show result
 
-        # Calculate how much percentage is HTTPS.
-        secure = 100 / len(f) * c
-
-        print(table) # Show result
-
-        print("\033[32mFinished checking {} domains in {} of which {}% has HTTPS.\033[0m".format(len(f), datetime.datetime.now()-start, secure))
+            print("\033[32mFinished checking {} domains in {} of which {} ({}%) has HTTPS.\033[0m".format(len(f), datetime.datetime.now()-start, c, secure))
+            sys.exit(0) # Done, exit script.
 
 
     except Exception as e:
